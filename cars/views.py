129803618c1bfa938e88b7bc577aa4228f348b83
Milton Aparecido Gomes <milton_apgomes@hotmail.com>
2026-02-17 15:16:44 +0100

    Templates

--- cars/views.py
+++ cars/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
+from cars.models import Car
 
 
 
 def cars_view(request):
-    html = '''
-    <html>
-        <head>
-            <title> Meus carros </title>
-        </head>
-        <body>
-            <h1>Carros da PycodeBr</h1>
-             <h3>Só carro top!</h3>
-        </body>
-    </html>
-'''
-    return HttpResponse(html)
+    cars = Car.objects.all()
+ 
+
+    return render(
+        request,
+        'cars.html',
+        {'cars':cars},
+        )
