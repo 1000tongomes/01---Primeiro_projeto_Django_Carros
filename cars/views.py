1c7af182765001f64049ce50d8b4ddf6db17879e
Milton Aparecido Gomes <milton_apgomes@hotmail.com>
2026-02-20 12:51:36 +0100

    20_02_26_Primeiro_Form

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.forms import CarForm
 
 
 
@@ -16,3 +17,8 @@ def cars_view(request):
         'cars.html',
         {'cars':cars},
         )
+
+
+def new_car_view(request):
+  new_car_from = CarForm()
+  return render(request,'new_car.html',{'new_car_form':new_car_from})
